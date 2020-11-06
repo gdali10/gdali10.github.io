8710c014e8b38bc5268cb5e53927a7c8dba01d65
gdali10 <37337116+gdali10@users.noreply.github.com>
2020-11-05 23:19:14 -0500

    New javascript file

--- Midterm/midtermEvent.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("btnGet").addEventListener("click", function() {
-    document.getElementById(".container").innerHTML = "This is the submission for Gaurav Dali";
-  });
--- README.md
+++
@@ -1,4 +0,0 @@
-# gdali10.github.io
-<h1>Gaurav's INST377 Spring Test Page</h1>
-<img src="https://graphiccreationlab.weebly.com/uploads/5/1/1/2/51125965/7924803_orig.jpg" 
-alt="Cheetah!"/>
