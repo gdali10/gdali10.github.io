3551a392bb007e299169c0f96913dc9d0f32afc5
gdali10 <37337116+gdali10@users.noreply.github.com>
2020-11-25 11:34:28 -0500

    new files for lab 8

--- Lab_8/public/js/lab8script.js
+++ Lab_8/public/js/lab8script.js
@@ -2,9 +2,11 @@
 let url = "https://www.reddit.com/r/popular.json";
 let chart = "";
 console.log(url);
+//const fetchPromise = fetch(url);
 
 let settings = { method: "Get" };
 let chartValues = [];
+
 
 async function getData() {
     await fetch(url, settings)
@@ -13,23 +15,38 @@ async function getData() {
             let listSize = json.data.children.length;
             // Loop to pick 5 random entries
             for (x = 0; x < 5; x++) {
+                //Get a random number within the size of the list
+
+
+                let randData = Math.floor(Math.random()*listSize);
+                let subreddit = json.data.children[randData].data.subreddit;
+                let author = json.data.children[randData].data.author;
+                let title = json.data.children[randData].data.title;
+                let ups = json.data.children[randData].data.ups;
+
+                let message = "<b>Subreddit </b>: " + subreddit + " <b>Author</b>:" + author + " <b>Title</b>:" + title + " <b>Up votes</b>: " + ups;
+                document.getElementById("redditlist").innerHTML += message;
+                console.log(subreddit, " ", author, " ", title, " ", ups);
                 /*
-                    Get a random number within the size of the list
                     Get subreddit, author, title, and ups from record
                     Set the message to be:
                         let message = "<b>Subreddit </b>: " + subreddit + " <b>Author</b>:" + author + " <b>Title</b>:" + title + " <b>Up votes</b>: " + ups;
                     Add a new <li> element with the message to the 'redditList' element
                     Add a data entry to chartValues with author as the label and ups as the y component
                 */
-    
+                let select = document.getElementById("redditlist");
+                select.innerHTML += "<li>" + message + "</li>";
+    //////////////////////////////////////////////////////////////////////
 
+                /*.......*/
                 
                 let addToChart = {'label':author,y:ups}; // Gave this. This needs to be added to the 'chartValues'
-            
+                /*.......*/
+                chartValues.push(addToChart);
             }
         })
         .then(values => console.log(chartValues));
-        //chart.render(); // Do you need to remove the comments from here in order to get it to work?
+        chart.render(); // Do you need to remove the comments from here in order to get it to work?
 };
 
 window.onload = async function makeChart() {
@@ -43,7 +60,7 @@ window.onload = async function makeChart() {
             { 
                 type: "column",
                 name: "Popular Reddit",
-                dataPoints: // WHAT GOES HERE???
+                dataPoints: chartValues // WHAT GOES HERE???
             }
         ]
     });
